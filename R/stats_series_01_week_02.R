3c63bb5a84d29fa89fd9af9db2998fd86663ef96
Jayson Danton <jayson.danton@me.com>
2023-07-29 14:03:48 +0200

    Cleaned up

--- R/stats_series_01_week_02.R
+++ R/stats_series_01_week_02.R
@@ -1,11 +1,11 @@
-# Day 1
+# Snippet 1
 # Discrete data
 pets <- sample(0:4, 10, replace = TRUE)
 
 # Continuous data
 weights <- rnorm(10, mean=70, sd=10)
 
-# Day 2
+# Snippet 2
 # Ordinal data
 sizes <- factor(c("Small", "Medium", "Large", "Large", "Small", "Medium"),
                 levels = c("Small", "Medium", "Large"), ordered = TRUE)
@@ -13,11 +13,11 @@ sizes <- factor(c("Small", "Medium", "Large", "Large", "Small", "Medium"),
 # Nominal data
 colors <- factor(c("Red", "Blue", "Green", "Blue", "Red", "Green"))
 
-# Day 3
+# Snippet 3
 # Binary data
 success <- sample(c(0, 1), size = 100, replace = TRUE, prob = c(0.5, 0.5))
 
-# Day 4
+# Snippet 4
 # Time series data
 library(xts)
 dates <- seq(as.Date("2020/1/1"), by = "day", length.out = 100)
